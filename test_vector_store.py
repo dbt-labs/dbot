65221e028361a43bbeae72e2af83a0c19f81351b
gwen windflower <gwenwindflower@gmail.com>
2023-04-26 16:41:03 -0500

    Fix old name for vector store dir in test

--- test_vector_store.py
+++ test_vector_store.py
@@ -5,7 +5,7 @@ from vector_store import VectorStore
 
 # def test_vector_store_returns_list_of_similar_documents_with_scores():
 #     vector_store = VectorStore(
-#         name="dbt-docs", sources_path="./test-docs", reindex=True
+#         name="qa-db", sources_path="./test-docs", reindex=True
 #     )
 
 #     results = vector_store.get_similar_documents("What is dbt?")
